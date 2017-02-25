39517386b21046c10425d0627d28e0d0f0eca309
Christoph Stach <christoph.stach@gmail.com>
2017-02-25 15:13:45 +0100

    first version

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+/**$('.carousel').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 3,
+  centerMode: true,
+  //centerPadding: '60px',
+  arrows: true,
+  dots: true,
+  variableWidth: false,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false
+      }
+    }
+  ]
+});*/
+
+//$('#carouselExampleSlidesOnly').carousel();
